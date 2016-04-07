015dae5a64ae6ea35225f05654466bf4b27b4d8d
nielsin <nielsin@student.matnat.uio.no>
2016-04-07 22:19:17 +0200

    Moved all static drawings to _make_empty

--- pi/DASHBOARD/dash.py
+++ pi/DASHBOARD/dash.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+'''
+Prerequisites:
+
+Pillow - PIL fork
+http://pillow.readthedocs.org
+'''
+
 from PIL import Image, ImageDraw, ImageFont
 from math import sin, cos, pi
 import numpy as np
@@ -8,27 +15,31 @@ import random
 class Dashboard(object):
 	"""docstring for Dashboard"""
 
+	def __init__(self, history=120):
+		self.timespan = history
+		self._make_empty()
+		self._clear_current_dash()
+
 	# Fonts
 	Ubuntu_R = ImageFont.truetype("fonts/Ubuntu-R.ttf", 15)
 	Ubuntu_L = ImageFont.truetype("fonts/Ubuntu-L.ttf", 15)
 	Ubuntu_M = ImageFont.truetype("fonts/Ubuntu-M.ttf", 15)
 	Ubuntu_B = ImageFont.truetype("fonts/Ubuntu-B.ttf", 15)
-
 	WIND_FONT = ImageFont.truetype("fonts/Ubuntu-B.ttf", 50)
 	MS_FONT = ImageFont.truetype("fonts/Ubuntu-R.ttf", 30)
 
-	def __init__(self):
-		self.make_empty()
-		self.clear_current_dash()
+	def _clear_current_dash(self):
+		self.current_dash = self.empty_dash.copy()
 
-	def make_empty(self):
+	def _set_wind(self, speed, direction):
+		self.wind_direction = direction
+		self.wind_speed = speed
+
+	def _make_empty(self, wind_history_ticks=5):
 		size=(600,250)
 		
 		# Create image
 		self.empty_dash = Image.new('L', size, color=0)
-
-		# Create draw object
-		draw = ImageDraw.Draw(self.empty_dash)
 
 		# circle_box
 		tl = (30, 30)		# top left corner
@@ -46,6 +57,9 @@ class Dashboard(object):
 		self.wind_dir_center = (h_mid, v_mid)
 		self.wind_dir_radius = (br[0] - tl[0]) / 2
 		self.wind_dir_bounding_box = [tl, br]
+
+		# Create draw object
+		draw = ImageDraw.Draw(self.empty_dash)
 
 		# Draw cross
 		width = 2
@@ -68,16 +82,40 @@ class Dashboard(object):
 		draw.text((3,18), u'Direction', fill=256, font=self.Ubuntu_R)
 
 		# Bounding box for wind speed
-		tl = (260, 120)		# top left corner
-		br = (500, 220)		# bottom right corner
+		tl = (330, 120)		# top left corner
+		br = (570, 220)		# bottom right corner
 
 		# Save parameters
 		self.wind_speed_bounding_box = [tl, br]
 
-		# Lines around avrage speed area
+		# Lines around speed history area
 		draw.line((tl, (br[0],tl[1])), fill=256, width=0)
 		draw.line(((tl[0],br[1]), br), fill=256, width=0)
 
+		# Wind history
+		tick_size = 5
+
+		tick_start = (tl[0],br[1])
+		tick_end = br
+		tick_line_len = tick_end[0] - tick_start[0]
+
+		for a in range(wind_history_ticks):
+			h_off = a*tick_line_len/(wind_history_ticks-1)
+			x = tick_start[0]+h_off
+			y1 = tick_start[1]
+			y2 = y1+tick_size
+			xy = ((x,y1),(x,y2))
+			draw.line(xy, fill=256, width=0)
+
+			text_str = str(a*self.timespan/(wind_history_ticks-1))
+
+			draw.text((x-5,y2+3), text_str, fill=256, font=self.Ubuntu_R)
+
+		# Write some annotation
+		text_str = 'Speed history last %s seconds' % self.timespan
+		draw.text((tl[0],tl[1]-20), text_str, fill=256, font=self.Ubuntu_R)
+		draw.text((tl[0]-40,br[1]+8), 'Age', fill=256, font=self.Ubuntu_R)
+		
 		# Draw line between speed and direction
 		draw.line(((380,10),(380,75)), fill=256, width=0)
 
@@ -85,50 +123,19 @@ class Dashboard(object):
 		draw.text((260,5), u'Direction', fill=256, font=self.Ubuntu_R)
 		draw.text((400,5), u'Speed', fill=256, font=self.Ubuntu_R)
 
+		# Sigma info
+		draw.text((3,210), u'Gray pie', fill=80, font=self.Ubuntu_R)
+		draw.text((3,230), u'1\u03C3 (68%)', fill=80, font=self.Ubuntu_R)
 
-	def draw_wind_speed_history(self):
-
-		self.timespan = 300
+	def _draw_wind_speed_history(self):
 
 		tl = self.wind_speed_bounding_box[0]
 		br = self.wind_speed_bounding_box[1]
 
 		# Create draw object
 		draw = ImageDraw.Draw(self.current_dash)
-
-		# Write some annotation
-		text_str = 'Speed history last %s seconds' % self.timespan
-		draw.text((tl[0],tl[1]-20), text_str, fill=256, font=self.Ubuntu_R)
-		draw.text((tl[0]-40,br[1]), 'Age', fill=256, font=self.Ubuntu_R)
-
-		# Ticks
-		tick_count = 5
-		tick_size = 5
-
-		tick_start = (tl[0],br[1])
-		tick_end = br
-		tick_line_len = tick_end[0] - tick_start[0]
-
-		for a in range(tick_count):
-			h_off = a*tick_line_len/(tick_count-1)
-			x = tick_start[0]+h_off
-			y1 = tick_start[1]
-			y2 = y1+tick_size
-			xy = ((x,y1),(x,y2))
-			draw.line(xy, fill=256, width=0)
-
-			text_str = str(self.timespan - (a*self.timespan/(tick_count-1)))
-
-			draw.text((x-5,y2+3), text_str, fill=256, font=self.Ubuntu_R)
 			
-	def clear_current_dash(self):
-		self.current_dash = self.empty_dash.copy()
-
-	def set_wind(self, speed, direction):
-		self.wind_direction = direction
-		self.wind_speed = speed
-
-	def print_wind_values(self):
+	def _print_wind_values(self):
 		# Create draw object
 		draw = ImageDraw.Draw(self.current_dash)
 
@@ -141,7 +148,7 @@ class Dashboard(object):
 		draw.text((400,20), spd_str, fill=256, font=self.WIND_FONT)
 		draw.text((500,40), 'm/s', fill=256, font=self.MS_FONT)
 
-	def draw_wind_arrow(self):
+	def _draw_wind_arrow(self, arrow_width=3):
 
 		arrow_len = self.wind_dir_radius*0.9
 		rad_wind_direction = self.wind_direction*pi/180
@@ -165,13 +172,13 @@ class Dashboard(object):
 		draw = ImageDraw.Draw(self.current_dash)
 
 		# Draw arrow line
-		draw.line([self.wind_dir_center, (u,v)], fill=256, width=2)
+		draw.line([self.wind_dir_center, (u,v)], fill=256, width=arrow_width)
 
 		# Draw arrow heads
-		draw.line([(lu,lv), (u,v)], fill=256, width=2)
-		draw.line([(ru,rv), (u,v)], fill=256, width=2)
+		draw.line([(lu,lv), (u,v)], fill=256, width=arrow_width)
+		draw.line([(ru,rv), (u,v)], fill=256, width=arrow_width)
 
-	def draw_wind_std_dev(self, values):
+	def _draw_wind_std_dev(self, values):
 		rad_values = values*np.pi/180
 		u = np.sin(rad_values)
 		v = np.cos(rad_values)
@@ -194,9 +201,6 @@ class Dashboard(object):
 
 		# Create draw object
 		draw = ImageDraw.Draw(self.current_dash)
-
-		draw.text((3,210), u'Gray pie', fill=30, font=self.Ubuntu_R)
-		draw.text((3,230), u'1\u03C3 (68%)', fill=30, font=self.Ubuntu_R)
 
 		# Draw pie
 		start = mean-std-90
@@ -233,22 +237,22 @@ def make_test_wind_values(num=100):
 if __name__ == '__main__':
 	d = Dashboard()
 
-	
+	'''
 	speed, direction = make_test_wind_values()
 
-	d.draw_wind_std_dev(direction)
+	d._draw_wind_std_dev(direction)
 
-	d.draw_wind_speed_history()
+	d._draw_wind_speed_history()
 
-	d.set_wind(speed=speed[-1], direction=direction[-1])
+	d._set_wind(speed=speed[-1], direction=direction[-1])
 
-	d.draw_wind_arrow()
+	d._draw_wind_arrow()
 
-	d.print_wind_values()
-	
+	d._print_wind_values()
+	'''
 
 
-	#d.empty_dash.show()
-	d.current_dash.show()
+	d.empty_dash.show()
+	#d.current_dash.show()
 
 	#d.current_dash.save('hei.png')
